6217bfcd229fec9fe803575f944b8e0ab4867e6c
odyyseus7 <pikaultra69@gmail.com>
2025-12-11 10:52:45 -0600

    Update main.blocks, main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
+namespace SpriteKind {
+    export const button = SpriteKind.create()
+    export const scorekeep = SpriteKind.create()
+}
 controller.combos.attachCombo("uuddlrlrba", function () {
     scene.setBackgroundImage(assets.image`voxbg`)
 })
@@ -5,11 +9,16 @@ browserEvents.onMouseMove(function (x, y) {
     mouse.setPosition(x, y)
 })
 let mouse: Sprite = null
-mouse = sprites.create(assets.image`cursor`, SpriteKind.Player)
+let bonesbg = sprites.create(assets.image`bonecount`, SpriteKind.scorekeep)
+let multbg = sprites.create(assets.image`multcount`, SpriteKind.scorekeep)
+bonesbg.setPosition(39, 55)
+multbg.setPosition(75, 55)
 scene.setBackgroundImage(assets.image`overseebg`)
 game.splash("Gambling", "(also known as betting)")
 game.splash("is the wagering of", "something of value")
+game.splash("you will wager your life")
 browserEvents.setCursorVisible(false)
+mouse = sprites.create(assets.image`cursor`, SpriteKind.Player)
 forever(function () {
     if (browserEvents.MouseLeft.isPressed()) {
         mouse.setImage(assets.image`click`)
